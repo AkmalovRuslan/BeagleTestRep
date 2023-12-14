0a4937d97635f884c9fd01b35cde2c4c212ab547
Ruslan <Ruslonsdump@gmail.com>
2023-12-14 16:30:58 +0500

    change version

--- helloworld.c
+++ helloworld.c
@@ -6,7 +6,7 @@ int main(void)
     unsigned char var = 0;
     while(1)
     {
-        printf("Hello from git %d\r\n", var++);
+        printf("Hello from git 1.1 %d\r\n", var++);
         usleep(100000);
     }
 }
